9c18ef61e2220f2bcc50290714abfd3bb926c4d6
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2021-11-10 02:04:01 +0000

    simple navigation bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import './App.css';
-import {Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <nav style={{ margin: 10 }}>
+    <Link to="/" style={{ padding: 5 }}>
+      Home
+    </Link>
+    <Link to="/choose" style={{ padding: 5 }}>
+      Submit Food Choices
+    </Link>
+    <Link to="/records" style={{ padding: 5 }}>
+      What did I choose?
+    </Link>
+  </nav>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/choose" element={<Choose />} />
+        <Route path="/records" element={<Records />} />
+      </Routes>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
       <header className="App-header">
         <h1>Christmas Lunch!</h1>
         <h2>Sloans</h2>
         <h2>December 16</h2>
         <h2>14.00</h2>
-        <Link to="/Dashboard"> Choose food </Link>
       </header>
+    </div>
+  );
+}
+
+function Choose() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Choose</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function Records() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>What did I choose?</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
     </div>
   );
 }
