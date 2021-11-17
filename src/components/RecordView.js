be7666bfacb9f5e235e27615bc6b0b06bddbf33e
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2021-11-17 00:57:59 +0000

    displaying full dish names in records

--- src/components/RecordView.js
+++ src/components/RecordView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import choices  from './consts';
 
 const printGF = (booleanToPrint) =>{
   if (booleanToPrint == undefined){
@@ -8,6 +9,22 @@ const printGF = (booleanToPrint) =>{
     return (<div>GF</div>)
   }
   return (<div>NOT GF</div>);
+}
+
+const printStarter = (optionName) => {
+  return findDescriptionFromList(optionName, choices.starters);
+}
+
+const printMain = (optionName) => {
+  return findDescriptionFromList(optionName, choices.mains);
+}
+
+const printDessert = (optionName) => {
+  return findDescriptionFromList(optionName, choices.desserts);
+}
+
+const findDescriptionFromList = (optionName, optionList) => {
+    return optionList.find(element => {return element.option === optionName}).description;
 }
 
 class RecordView extends React.Component {
@@ -22,20 +39,20 @@ class RecordView extends React.Component {
     return (
       <div>
         <div>
-      <div>Starter:</div>
-      <div>{this.props.data.starter}</div>
-      {printGF(this.props.data.starterGF)}
-      </div>
-      <div>
-      <div>Main:</div>
-      <div>{this.props.data.main}</div>
-      {printGF(this.props.data.mainGF)}
-      </div>
-      <div>
-      <div>Dessert:</div>
-      <div>{this.props.data.dessert}</div>
-      {printGF(this.props.data.dessertGF)}
-      </div>
+          <div>Starter:</div>
+          <div>{printStarter(this.props.data.starter)}</div>
+          {printGF(this.props.data.starterGF)}
+        </div>
+        <div>
+          <div>Main:</div>
+          <div>{printMain(this.props.data.main)}</div>
+          {printGF(this.props.data.mainGF)}
+        </div>
+        <div>
+          <div>Dessert:</div>
+          <div>{printDessert(this.props.data.dessert)}</div>
+          {printGF(this.props.data.dessertGF)}
+        </div>
       </div>
     )
   }
